d62f04ede38c55da13733914bb8fc63a600c48db
Joel Knight <knight.joel@gmail.com>
2017-10-27 13:44:59 -0600

    Simplify repetitive code in the test cases
    - Individual test cases are a little cleaner now
    - The mocks for submitting a task to the queue and for talking to the
    Spark API are now always present, even for future test cases

--- zpark/testing.py
+++ zpark/testing.py
@@ -28,74 +28,71 @@ class BaseTestCase(TestCase):
         pass
 
 
+@patch('zpark.tasks.task_send_spark_message.apply_async', autospec=True)
 class ApiV1TestCase(BaseTestCase):
 
     ### GET /alert endpoint
-    def test_alert_get_w_token(self):
+    def test_alert_get_w_token(self, *args):
         r = self.client.get(url_for('api_v1.alert'),
                             headers=[self.sb_api_token])
 
         self.assert_405(r)
 
-    def test_alert_get_wo_token(self):
+    def test_alert_get_wo_token(self, *args):
         r = self.client.get(url_for('api_v1.alert'))
 
         self.assert_405(r)
 
     ### POST /alert endpoint
-    def test_alert_post_valid_alert_direct(self):
+    def test_alert_post_valid_alert_direct(self, mock_task):
         to = u'joel@zpark.packetmischief'
         subject = u'This might ruin your day...'
         message = u'Your data center is on fire'
 
-        with patch('zpark.tasks.task_send_spark_message.apply_async') \
-                as mock_task:
-            type(mock_task.return_value).id = PropertyMock(
-                                                return_value='id123abc')
+        type(mock_task.return_value).id = PropertyMock(
+                                            return_value='id123abc')
 
-            r = self.client.post(url_for('api_v1.alert'),
-                                 headers=[self.sb_api_token],
-                                 data=json.dumps({
-                                    'to': to,
-                                    'subject': subject,
-                                    'message': message
-                                 }),
-                                 content_type='application/json')
-            self.assert_200(r)
-            mock_task.assert_called_once()
-            rjson = json.loads(r.data)
-            self.assertEqual(rjson['message'], '{}\n\n{}'.format(subject, message))
-            self.assertEqual(rjson['toPersonEmail'], to)
-            self.assertIsNone(rjson['toRoomId'])
-            self.assertEqual(rjson['taskid'], 'id123abc')
+        r = self.client.post(url_for('api_v1.alert'),
+                             headers=[self.sb_api_token],
+                             data=json.dumps({
+                                'to': to,
+                                'subject': subject,
+                                'message': message
+                             }),
+                             content_type='application/json')
+        self.assert_200(r)
+        mock_task.assert_called_once()
+        rjson = json.loads(r.data)
+        self.assertEqual(rjson['message'], '{}\n\n{}'.format(subject, message))
+        self.assertEqual(rjson['toPersonEmail'], to)
+        self.assertIsNone(rjson['toRoomId'])
+        self.assertEqual(rjson['taskid'], 'id123abc')
 
-    def test_alert_post_valid_alert_group(self):
+    def test_alert_post_valid_alert_group(self, mock_task):
         to = u'roomid1234567'
         subject = u'This might ruin your day...'
         message = u'Your data center is on fire'
 
-        with patch('zpark.tasks.task_send_spark_message.apply_async') \
-                as mock_task:
-            type(mock_task.return_value).id = PropertyMock(
-                                                return_value='id123abc')
+        type(mock_task.return_value).id = PropertyMock(
+                                            return_value='id123abc')
 
-            r = self.client.post(url_for('api_v1.alert'),
-                                 headers=[self.sb_api_token],
-                                 data=json.dumps({
-                                    'to': to,
-                                    'subject': subject,
-                                    'message': message
-                                 }),
-                                 content_type='application/json')
-            self.assert_200(r)
-            mock_task.assert_called_once()
-            rjson = json.loads(r.data)
-            self.assertEqual(rjson['message'], '{}\n\n{}'.format(subject, message))
-            self.assertEqual(rjson['toRoomId'], to)
-            self.assertIsNone(rjson['toPersonEmail'])
-            self.assertEqual(rjson['taskid'], 'id123abc')
+        r = self.client.post(url_for('api_v1.alert'),
+                             headers=[self.sb_api_token],
+                             data=json.dumps({
+                                'to': to,
+                                'subject': subject,
+                                'message': message
+                             }),
+                             content_type='application/json')
+        self.assert_200(r)
+        mock_task.assert_called_once()
+        rjson = json.loads(r.data)
+        self.assertEqual(rjson['message'], '{}\n\n{}'.format(subject, message))
+        self.assertEqual(rjson['toRoomId'], to)
+        self.assertIsNone(rjson['toPersonEmail'])
+        self.assertEqual(rjson['taskid'], 'id123abc')
 
-    def test_alert_post_valid_alert_wo_token(self):
+    def test_alert_post_valid_alert_wo_token(self, *args):
         to = u'joel@zpark.packetmischief'
         subject = u'This might ruin your day...'
         message = u'Your data center is on fire'
@@ -118,7 +115,7 @@ class ApiV1TestCase(BaseTestCase):
         self.assert_status(r, 400)
         self.assertIn(b'Required', r.data)
 
-    def test_alert_post_missing_to(self):
+    def test_alert_post_missing_to(self, *args):
         input_ = {
             # missing 'to'
             'subject': 'subj',
@@ -127,7 +124,7 @@ class ApiV1TestCase(BaseTestCase):
 
         self._alert_post_missing_input(input_)
 
-    def test_alert_post_missing_subject(self):
+    def test_alert_post_missing_subject(self, *args):
         input_ = {
             # missing 'subject'
             'to': 'joel',
@@ -136,7 +133,7 @@ class ApiV1TestCase(BaseTestCase):
 
         self._alert_post_missing_input(input_)
 
-    def test_alert_post_missing_message(self):
+    def test_alert_post_missing_message(self, mock_task):
         """
         'message' is allowed to be absent so the result of this test should be
         an HTTP 200 and a good status message returned from the API.
@@ -147,40 +144,39 @@ class ApiV1TestCase(BaseTestCase):
             'subject': 'subj',
         }
 
-        with patch('zpark.tasks.task_send_spark_message.apply_async') \
-                as mock_task:
-            type(mock_task.return_value).id = PropertyMock(
-                                                return_value='id123abc')
-            r = self.client.post(url_for('api_v1.alert'),
-                                 headers=[self.sb_api_token],
-                                 data=json.dumps(input_),
-                                 content_type='application/json')
-            self.assert_200(r)
-            rjson = json.loads(r.data)
-            self.assertEqual(rjson['message'], input_['subject'])
+        type(mock_task.return_value).id = PropertyMock(
+                                            return_value='id123abc')
+        r = self.client.post(url_for('api_v1.alert'),
+                             headers=[self.sb_api_token],
+                             data=json.dumps(input_),
+                             content_type='application/json')
+        self.assert_200(r)
+        rjson = json.loads(r.data)
+        self.assertEqual(rjson['message'], input_['subject'])
 
     ### /ping endpoint
-    def test_ping_get_wo_token(self):
+    def test_ping_get_wo_token(self, *args):
         r = self.client.get(url_for('api_v1.ping'))
 
         self.assert_401(r)
 
-    def test_ping_get_w_token(self):
+    def test_ping_get_w_token(self, *args):
         r = self.client.get(url_for('api_v1.ping'),
                             headers=[self.sb_api_token])
 
         self.assert_200(r)
         self.assertEqual(json.loads(r.data)['apiversion'], zpark.v1.API_VERSION)
 
-    def test_ping_post_verb(self):
+    def test_ping_post_verb(self, *args):
         r = self.client.post(url_for('api_v1.ping'))
 
         self.assert_405(r)
 
 
+@patch('zpark.spark_api.messages.create', autospec=True)
 class TaskTestCase(BaseTestCase):
 
-    def test_task_send_spark_message(self):
+    def test_task_send_spark_message(self, mock_sparkapi):
         to = u'joel@zpark.packetmischief'
         subject = u'This might ruin your day...'
         message = u'Your data center is on fire'
@@ -196,10 +192,7 @@ class TaskTestCase(BaseTestCase):
             text='\n\n'.join([subject, message])
         )
 
-        mock_sparkapi = patch('zpark.spark_api.messages.create',
-                              autospec=True)
-        mock_sparkapi_patcher = mock_sparkapi.start()
-        mock_sparkapi_patcher.return_value = my_spark_output
+        mock_sparkapi.return_value = my_spark_output
 
         my_spark_msg = dict(
             toPersonEmail=to,
@@ -208,19 +201,15 @@ class TaskTestCase(BaseTestCase):
 
         self.assertEqual(my_spark_output.id,
                          zpark.tasks.task_send_spark_message(my_spark_msg))
-        mock_sparkapi.stop()
 
-    def test_task_send_spark_message_retry(self):
+    def test_task_send_spark_message_retry(self, mock_sparkapi):
         to = u'joel@zpark.packetmischief'
         subject = u'This might ruin your day...'
         message = u'Your data center is on fire'
 
         e = SparkApiError(429)
 
-        mock_sparkapi = patch('zpark.spark_api.messages.create',
-                              autospec=True)
-        mock_sparkapi_patcher = mock_sparkapi.start()
-        mock_sparkapi_patcher.side_effect = [e, None]
+        mock_sparkapi.side_effect = [e, None]
         mock_retry = patch('zpark.tasks.task_send_spark_message.retry',
                            autospec=True)
         mock_retry_patcher = mock_retry.start()
@@ -237,5 +226,4 @@ class TaskTestCase(BaseTestCase):
         mock_retry_patcher.assert_called_with(exc=e)
 
         mock_retry.stop()
-        mock_sparkapi.stop()
 
